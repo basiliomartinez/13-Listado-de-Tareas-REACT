5976678e754622c8c18fff3bdfe3781f657f9517
basiliomartinez <basilioezequielmartinez@gmail.com>
2025-10-31 22:51:08 -0300

    Agregamos validaciones para no cargar dos tareas iguales

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -2,13 +2,20 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import ListaTareas from "./ListaTareas";
 import { useState } from "react";
+import ItemTarea from "./ItemTarea";
 
 const FormularioTarea = () => {
     const [arrayTareas, setArrayTareas]= useState([])
     const[tarea, setTarea]= useState('')
 const handlesubmit= (e)=>{e.preventDefault()
+    //Verificar que la tarea no este repetida
+    const tareaBuscada= arrayTareas.find((itemTarea)=>itemTarea.toLowerCase()===tarea.toLowerCase().trim())
+    console.log(tareaBuscada)
+    if(tareaBuscada){
+        return alert('Ya existe una tarea con ese nombre')
+    }
 //tomar el valor del state tarea y almacenarlo en el arrayTareas
-setArrayTareas([...arrayTareas, tarea])
+setArrayTareas([...arrayTareas, tarea.trim()])
 //limpiar el state tarea
 setTarea('')
 
